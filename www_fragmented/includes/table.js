6624a88bed265826af55e243404a214585b3b31f
Toms <toms.nimanis@gmail.com>
2020-07-14 00:50:27 +0300

    20200714_00_50

---
+++ www_fragmented/includes/table.js
@@ -0,0 +1,1 @@
+document.write("<h2>Table</h2><table><tr><th>Vārds</th><th>Uzvārds</th><th>Vecums</th></tr><tr><td>Jānis</td><td>Bērziņš</td><td>30</td></tr><tr><td>Anna</td><td>Liepiņa</td><td>29</td></tr></table>");
