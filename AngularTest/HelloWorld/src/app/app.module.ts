a75367497b6c60a076140d9baa1af541ee30bd50
Toms Nīmanis <toms.nimanis@gmail.com>
2020-07-20 17:09:29 +0300

    20200720_17_09

--- AngularTest/HelloWorld/src/app/app.module.ts
+++ AngularTest/HelloWorld/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ProfileService} from './profile.service';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import{ DataService} from './data.service';
+import{CurrencyModule} from './currency/currency.module'
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    CurrencyModule
   ],
-  providers: [],
+  providers: [
+    ProfileService,
+    DataService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ CW/WEEK4/DAY1/readme.md
@@ -0,0 +1,4 @@
+# Angular get data from server and edit it
+[Angular Tutorial for api editing](https://angular.io/tutorial/toh-pt6)
+
+
