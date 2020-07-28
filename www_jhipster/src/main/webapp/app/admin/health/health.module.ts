cd4f79ad47370a422dfaec025e2df004df89924a
Toms Nīmanis <toms.nimanis@gmail.com>
2020-07-28 11:42:47 +0300

    Initial version of www_jhipster generated by JHipster-6.10.1

---
+++ www_jhipster/src/main/webapp/app/admin/health/health.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { WwwJhipsterSharedModule } from 'app/shared/shared.module';
+
+import { HealthComponent } from './health.component';
+import { HealthModalComponent } from './health-modal.component';
+
+import { healthRoute } from './health.route';
+
+@NgModule({
+  imports: [WwwJhipsterSharedModule, RouterModule.forChild([healthRoute])],
+  declarations: [HealthComponent, HealthModalComponent],
+  entryComponents: [HealthModalComponent],
+})
+export class HealthModule {}
